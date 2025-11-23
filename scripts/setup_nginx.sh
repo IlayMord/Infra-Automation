1cd1af2daf75909ca022710571a30aa9aabfac68
Ilay Mor <ilaym1000@gmail.com>
2025-11-23 11:50:45 +0200

    I improved the Bash script to be more relevant.

--- scripts/setup_nginx.sh
+++ scripts/setup_nginx.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 #
-#The script installs Nginx on the system while writing structured log entries for each step of the process.
+#The script installs Nginx on the system.
 
 #Log file location.
 log_file="$(dirname "$0")/../logs/nginx_script.log"
-script_name="$(basename "$0")"
 
 #logging format.
 log() {
     printf "%s - %s - %s - %s\n" \
         "$(date '+%Y-%m-%d %H:%M:%S,%3N')" \
         "$1" \
-        "$script_name" \
         "$2" >> "$log_file"
 }
 
